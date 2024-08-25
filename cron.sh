8fa481fadf8b934da43c7bb5201e101f7342781f
James Lemin <jameslemin@duck.com>
2024-08-25 08:51:15 +0100

    Auto commit: 2024-08-25

--- cron.sh
+++ cron.sh
@@ -1,32 +1,42 @@
 #!/usr/bin/env bash
 # This script is intended to be run as a cron job at 23:55 daily
 ###  DESCRIPTION    ###########################################################
-# + count how many words have been cumulatively written
+# + count how many words have been cumulatively written in _numbered_ markdown files
 # + prettify all files in the directory
 # + list outstanding TODOS in the markdown files
 # + commit the changes to the git repository
 
 ###  VARIABLES      ###########################################################
-_OUTFILE="README.md"
+_README="README.md"
 _TODAY=$(date +%Y-%m-%d)
 _TODOS="TODOS.md"
+_TODO_FILTER="<!-- TODO:.*-->"
 
 ###  FUNCTIONS      ###########################################################
 
+_get_files() {
+    # use the same file filter throughout...
+    find . -regex '^\.\/[0-9][0-9]\/.*[0-9]\.md'
+}
+
 _get_wc() {
-    cat [0-9]*/*.md | wc -w
+    cat $(_get_files) | wc -w
+}
+
+_get_todo_count() {
+    # prints the number of TODOs in the markdown files
+    cat $(_get_files) | grep -c "$_TODO_FILTER"
+}
+
+_set_progress() {
+    OUTFILE=$1
+    _WC=$(_get_wc)
+    _TODO=$(_get_todo_count)
+    echo "|" "$_TODAY" "|" "$_WC" "| [""$_TODO""](TODOS.md) |" >>$OUTFILE
 }
 
 _todo_headers() {
     printf "\n| File | TODO |\n|------|------|\n"
-}
-
-_todo_count() {
-    # prints the number of TODOs in the markdown files in a handy-danndy
-    # link to the TODOS.md file
-    shopt -s globstar
-    echo "["$(grep -o "TODO:" **/*.md | wc -l)"](TODOS.md)"
-    shopt -u globstar
 }
 
 _list_todos() {
@@ -34,12 +44,32 @@ _list_todos() {
     # strips <!-- TODO: and --> from the output
     # Returns: | <file> | <todo> |
     _todo_headers
-    find . -name "*.md" | while read -r file; do
-        grep -o "<!-- TODO:.*-->" "$file" | sed -e 's/<!-- TODO: //g' -e 's/ -->//g' |
+    _get_files | while read -r file; do
+        grep -o "$_TODO_FILTER" "$file" | sed -e 's/<!-- TODO: //g' -e 's/ -->//g' |
             while read -r line; do
                 echo "| [""$file""](""$file"") |" "$line" "|"
             done
     done | sort
+}
+
+_build_todos() {
+    TODOS=$1
+    # build the TODOS.md file
+    _list_todos >$TODOS
+}
+
+_prettify() {
+    # prettify all the markdown files in the directory
+    files=$(_get_files)
+    for file in $files; do
+        npx prettier --write "$file"
+    done
+}
+
+_prettify_outputs() {
+    # prettify the outputs of the script
+    npx prettier --write "$_TODOS"
+    npx prettier --write "$_README"
 }
 
 _git_commit() {
@@ -49,11 +79,31 @@ _git_commit() {
 }
 
 ###  MAIN           ###########################################################
-_WC=$(_get_wc)
-_TODO=$(_todo_count)
 
-echo "|" "$_TODAY" "|" "$_WC" "|" "$_TODO" "|" >>$_OUTFILE
-_list_todos >$_TODOS
-find . -name "*.md" -exec npx prettier --write {} +
-sleep 2
-_git_commit
+
+_MAIN() {
+    while getopts "wtug" opt; do
+        case $opt in
+        w)
+            echo "Word count: $(_get_wc)"
+            ;;
+        t)
+            echo "TODO count: $(_get_todo_count)"
+            ;;
+        u)
+            _set_progress $_README
+            _build_todos $_TODOS
+            _prettify
+            _prettify_outputs
+            ;;
+        g)
+            _git_commit
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+        esac
+    done
+}
+
+_MAIN "$@"
